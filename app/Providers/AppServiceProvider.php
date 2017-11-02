48e592e155d0d4a377bd976333d7c4e15bb8101d
João Azevedo <joaopedro7_11@hotmail.com>
2017-11-02 11:09:32 +0000

    Fix | AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Schema::defaultStringLength(191);
     }
 
     /**
@@ -28,4 +28,6 @@ class AppServiceProvider extends ServiceProvider
             $this->app->register(DuskServiceProvider::class);
         }
     }
+
+
 }
