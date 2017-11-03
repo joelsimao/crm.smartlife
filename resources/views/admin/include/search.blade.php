d271b8bcb28053bedea0253e8b00c3784c82aa69
João Azevedo <joaopedro7_11@hotmail.com>
2017-11-03 11:16:02 +0000

    Fix | Search bar visualization

--- resources/views/admin/include/search.blade.php
+++ resources/views/admin/include/search.blade.php
@@ -1,25 +1,27 @@
 <div class="row">
-    <div class="col-lg-4">
-        <div class="input-group date datepicker" data-provide="datepicker">
-            {{ Form::text('start_date', isset($ds) && $ds!= '' ? $ds : null, array('class' => 'form-control', 'id' => 'ds')) }}
-            <div class="input-group-addon">
-                <span class="glyphicon glyphicon-th"></span>
+    <div class="form-group">
+        <div class="col-lg-4">
+            <div class="input-group date datepicker" data-provide="datepicker">
+                {{ Form::text('start_date', isset($ds) && $ds!= '' ? $ds : null, array('class' => 'form-control', 'id' => 'ds')) }}
+                <div class="input-group-addon">
+                    <span class="glyphicon glyphicon-th"></span>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="col-lg-4">
-        <div class="input-group date datepicker" data-provide="datepicker">
-            {{ Form::text('end_date', isset($de) && $de!= '' ? $de : null, array('class' => 'form-control', 'id' => 'de')) }}
-            <div class="input-group-addon">
-                <span class="glyphicon glyphicon-th"></span>
+        <div class="col-lg-4">
+            <div class="input-group date datepicker" data-provide="datepicker">
+                {{ Form::text('end_date', isset($de) && $de!= '' ? $de : null, array('class' => 'form-control', 'id' => 'de')) }}
+                <div class="input-group-addon">
+                    <span class="glyphicon glyphicon-th"></span>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="col-lg-3">
-        {{ Form::text('search', isset($search) && $search!= '' ? $search : null, array('class' => 'form-control', 'id' => 'search')) }}
-    </div>
-    <div class="col-lg-1">
-        <button type="button" id="search_btn">Pesquisar</button>
+        <div class="col-lg-3">
+            {{ Form::text('search', isset($search) && $search!= '' ? $search : null, array('class' => 'form-control', 'id' => 'search')) }}
+        </div>
+        <div class="col-lg-1">
+            <button type="button" id="search_btn">Pesquisar</button>
+        </div>
     </div>
 </div>
 
