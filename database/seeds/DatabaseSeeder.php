edddb46f44c5c08452d05ad603406f27777b9f1e
João Azevedo <joaopedro7_11@hotmail.com>
2017-11-13 01:40:57 +0000

    Fix | jobs.csv && DatabaseSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,8 +13,8 @@ class DatabaseSeeder extends Seeder
     {
          $this->call([
              MaritalStatusSeeder::class,
-             JobSeeder::class,
              RolePermissionSeeder::class,
+             JobSeeder::class,
              //UsersTableSeeder::class
          ]);
     }
