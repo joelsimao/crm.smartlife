55c9b3ada3e9d755951a44a9b5b14e6ef2dabe1c
João Azevedo <joaopedro7_11@hotmail.com>
2017-11-13 10:35:43 +0000

    New Feature | AgencySeeder

---
+++ database/seeds/AgencySeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Agency;
+use Illuminate\Database\Seeder;
+
+class AgencySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        if(App::environment('production')){
+            $file = 'C:\inetpub\vhosts\smartlife.pt\crm.smartlife.pt\storage\app\csv\agencies.csv';
+        } else {
+            $file = storage_path('app/csv/agencies.csv');
+        }
+
+        $agencies=csvToArray($file);
+        foreach ($agencies as $agency){
+            Agency::create($agency);
+        }
+    }
+}
