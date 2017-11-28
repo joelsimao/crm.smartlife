e321aa2ee3495a6628aecfbbd4033b5a82852973
João Azevedo <joaopedro7_11@hotmail.com>
2017-11-28 11:12:42 +0000

    Fix | Club1970 homepage

--- resources/views/admin/homepages/club.blade.php
+++ resources/views/admin/homepages/club.blade.php
@@ -5,14 +5,12 @@
 @endsection
 
 @section('main-content')
-    @if(App::environment()=="local")
-        <div class="col-md-4 col-md-offset-2">
-            <a href="/client/create"><i class='fa fa-plus-square fa-5x' aria-hidden="true" style="vertical-align: middle;"></i></a>
-            {{--<span> Adicionar Cliente</span>--}}
-        </div>
-        <div class="col-md-4 col-md-offset-2">
-            <a href="client"><i class='fa fa-search fa-5x' aria-hidden="true" style="vertical-align: middle;"></i></a>
-            {{--<span> Pesquisar Cliente</span>--}}
-        </div>
-    @endif
+    <div class="col-md-4 col-md-offset-2">
+        <a href="/client/create"><i class='fa fa-plus-square fa-5x' aria-hidden="true" style="vertical-align: middle;"></i></a>
+        {{--<span> Adicionar Cliente</span>--}}
+    </div>
+    <div class="col-md-4 col-md-offset-2">
+        <a href="client"><i class='fa fa-search fa-5x' aria-hidden="true" style="vertical-align: middle;"></i></a>
+        {{--<span> Pesquisar Cliente</span>--}}
+    </div>
 @endsection
