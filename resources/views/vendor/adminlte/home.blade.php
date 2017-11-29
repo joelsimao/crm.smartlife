73d42a7fa1b64567d6a44347f89a44b78bdfb775
João Azevedo <joaopedro7_11@hotmail.com>
2017-11-29 11:14:00 +0000

    Fix | home main-page

--- resources/views/vendor/adminlte/home.blade.php
+++ resources/views/vendor/adminlte/home.blade.php
@@ -7,8 +7,8 @@
 
 @section('main-content')
 	<div class="container-fluid spark-screen">
-		<div class="row">
-			@if(App::environment()=="local")
+
+			{{--@if(App::environment()=="local")--}}
 				{{--<div class="col-md-4 col-md-offset-2">--}}
 						{{--<a href="/client/create"><i class='fa fa-plus-square fa-5x' aria-hidden="true" style="vertical-align: middle;"></i></a>--}}
 						{{--<span> Adicionar Cliente</span>--}}
@@ -17,55 +17,75 @@
 					{{--<a href="client"><i class='fa fa-search fa-5x' aria-hidden="true" style="vertical-align: middle;"></i></a>--}}
 					{{--<span> Pesquisar Cliente</span>--}}
 				{{--</div>--}}
-			@endif
-				<div class="col-md-4 col-md-offset-2" align="center">
-					<label>Últimos acessos</label>
-					<table class="table">
-						<thead>
-							<tr class="info">
-								<th style="text-align:center">{{Auth::user()->name}}</th>
-							</tr>
+			{{--@endif--}}
+			<div class="col-md-10 col-md-offset-1">
+				<div class="row">
+					<div class="box">
+						<div class="box-header with-border">
+							<h3 class="box-title"></h3>
+							<div class="box-tools pull-right">
+								{{--<button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="Collapse">--}}
+									{{--<i class="fa fa-minus"></i></button>--}}
+							</div>
+						</div>
+						<div class="box-body">
+							{{--{{ trans('adminlte_lang::message.logged') }}. Start creating your amazing application!--}}
+							<div class="row">
+								<div class="col-md-4 col-md-offset-2" align="center">
+									<label>Últimos acessos</label>
+									<table class="table">
+										<thead>
+										<tr class="info">
+											<th style="text-align:center">{{Auth::user()->name}}</th>
+										</tr>
 
-						</thead>
-						<tbody>
-						<tr style="background-color: #FFFFFF">
-							<td>Registo de acesso</td>
-						</tr>
+										</thead>
+										<tbody>
+										<tr style="background-color: #FFFFFF">
+											<td>Registo de acesso</td>
+										</tr>
 
-						</tbody>
-					</table>
-					@php
-						$date_update = new \Carbon\Carbon(Auth::user()->updated_at);
-						$diff = $date_update->diffInDays(\Carbon\Carbon::now());
-					@endphp
-					<div align="justify">
-						<span><b>Última atualização</b> {{$date_update->format("d-m-Y")." (".$diff." dias)"}}</span>
+										</tbody>
+									</table>
+									@php
+										$date_update = new \Carbon\Carbon(Auth::user()->updated_at);
+                                        $diff = $date_update->diffInDays(\Carbon\Carbon::now());
+									@endphp
+									<div align="justify">
+										<span><b>Última atualização</b> {{$date_update->format("d-m-Y")." (".$diff." dias)"}}</span>
+									</div>
+								</div>
+								<div class="col-md-4" align="center">
+									<label>Últimas Mensagens</label>
+									<table class="table">
+										<thead>
+										<tr class="info">
+											<th>Colaborador</th>
+											<th>Data</th>
+											<th>Assunto</th>
+											<th>Sócio</th>
+										</tr>
+
+										</thead>
+										<tbody>
+										<tr style="background-color: #FFFFFF">
+											<td>C1</td>
+											<td>Data</td>
+											<td>Subject</td>
+											<td><a href="#">Nome de Sócio</a></td>
+										</tr>
+
+										</tbody>
+									</table>
+								</div>
+							</div>
+						</div>
+						<!-- /.box-body -->
 					</div>
 				</div>
+			</div>
 
-				<div class="col-md-4" align="center">
-					<label>Últimas Mensagens</label>
-					<table class="table">
-						<thead>
-						<tr class="info">
-							<th>Colaborador</th>
-							<th>Data</th>
-							<th>Assunto</th>
-							<th>Sócio</th>
-						</tr>
 
-						</thead>
-						<tbody>
-						<tr style="background-color: #FFFFFF">
-							<td>C1</td>
-							<td>Data</td>
-							<td>Subject</td>
-							<td><a href="#">Nome de Sócio</a></td>
-						</tr>
-
-						</tbody>
-					</table>
-				</div>
 
 
 				<!-- Default box -->
@@ -87,7 +107,6 @@
                 </div>--}}
 				<!-- /.box -->
 
-			</div>
 		</div>
 	</div>
 @endsection
