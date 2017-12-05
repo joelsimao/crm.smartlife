e3784ecd02f4b551df1903a47594dc4e32105da0
João Azevedo <joaopedro7_11@hotmail.com>
2017-12-05 15:30:18 +0000

    Update | phrases seeder

--- database/seeds/PhraseSeeder.php
+++ database/seeds/PhraseSeeder.php
@@ -11,6 +11,15 @@ class PhraseSeeder extends Seeder
      */
     public function run()
     {
-        //
+        if(App::environment(['production', 'development'])){
+            $file = 'C:\inetpub\vhosts\smartlife.pt\crm.smartlife.pt\storage\app\csv\phrases.csv';
+        } else {
+            $file = storage_path('app/csv/phrases.csv');
+        }
+
+        $agencies=csvToArray($file);
+        foreach ($agencies as $agency){
+            Agency::create($agency);
+        }
     }
 }
