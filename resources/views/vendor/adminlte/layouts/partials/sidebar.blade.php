197aaccb23e1ba1d05575e8eb0741a74e649c5b8
João Azevedo <joaopedro7_11@hotmail.com>
2017-12-07 12:02:04 +0000

    Fix sidebar

--- resources/views/vendor/adminlte/layouts/partials/sidebar.blade.php
+++ resources/views/vendor/adminlte/layouts/partials/sidebar.blade.php
@@ -10,13 +10,8 @@
                 <li class="treeview">
                     <a href="#"><i class='fa fa-address-card'></i> <span>Upsheets</span> <i class="fa fa-angle-left pull-right"></i></a>
                     <ul class="treeview-menu">
-                        @if(App::environment(['production', 'development'])){
-                            <li><a href="/client">Mostrar</a></li>
-                            <li><a href="/client/create">Inserir</a></li>
-                        @else
-                            <li><a href="/club/client">Mostrar</a></li>
-                            <li><a href="/club/client/create">Inserir</a></li>
-                        @endif
+                        <li><a href="/club/client">Mostrar</a></li>
+                        <li><a href="/club/client/create">Inserir</a></li>
 
                         <li class="treeview">
                             <a href="#"><i class='fa fa-briefcase'></i> <span>Profissões</span> <i class="fa fa-angle-left pull-right"></i></a>
